67e59fa78dc85a9a5fb68d1f2a60b063e020eebb
jecneps <jejcneps@gmail.com>
2019-06-25 14:55:09 -0400

    conversiona ad bulk edit going nice

--- src/front_end.js
+++ src/front_end.js
@@ -12,8 +12,21 @@ ipc.on("loading-event", function(event, args) {
     console.log(event)
     console.log(args)
     if (args) { loadState = LoadEnum.loading }
-    else { loadState = LoadEnum.start }
+    else {
+	loadState = LoadEnum.start
+	m.route.set("/bulkEdit")
+    }
     m.redraw()
+})
+
+ipc.on("error", function(event, args) {
+    console.log("EEERRROR")
+    loadState = LoadEnum.start
+    m.redraw()
+})
+
+ipc.on("openBulk", function(event, args) {
+    m.route.set("/bulkEdit")
 })
 
 function callipc(url, format) {
@@ -25,6 +38,67 @@ function callipc(url, format) {
 }
 
 
+function singleEditComponent() {
+    return {
+	view: function() {}
+    }
+}
+
+
+function bulkEditComponent() {
+    let artist = "";
+    let album = "";
+    let photo = null;
+    return {
+	view: function() {
+	    return m("div", {class: "flex flex-column bg-orange w-50", style: "margin: auto"}, [
+		m("h1", "Bulk Edit") ,
+		m("div", {class: "flex"}, [
+		    m("div", {class: "flex flex-column"}, [
+			m("p", "Artist"),
+			m("input", {type: "text",
+				    class: "mb4",
+				    onchange: function(e) {
+					e.redraw = false
+					artist = e.currentTarget.value
+				    }
+				   }),
+			m("p", "Album"),
+			m("input", {type: "text",
+				    class: "",
+				    onchange: function(e) {
+					e,redraw = false
+					album = e.currentTarget.value
+				    }
+				   })
+			
+		    ]),
+		    m("div", "Image thing")
+		]),
+		m("div", {class: "flex"}, [
+		    m("button", {class: "",
+				 onclick: function() {
+				     m.route.set("/singleEdit")
+				 }
+				},
+				 "Skip"),
+		      m("button", {class: "",
+				   onclick: function() {
+				       ipc.send("bulk-edit", {
+					   artist: artist,
+					   album: album,
+					   photo: photo
+				       })
+				   }
+				  },
+			"Perform")
+		])
+	    ])
+	}
+    }
+}
+
+
 
 function downloadComponent() {
     let url = ""
@@ -32,13 +106,16 @@ function downloadComponent() {
     return  {
 	view: function() {
 	    console.log(`load state ${loadState}`)
-	    var loadStyle = (loadState == LoadEnum.loading) ? "block" : "none"
-	    var submitStyle = (loadState == LoadEnum.loading) ? "none":"block"
+	    var loadStyle = (loadState == LoadEnum.loading) ? "display:block" : "display:none"
+	    var submitStyle = (loadState == LoadEnum.loading) ? "display:none":"display:block"
+	    console.log(`ls ${loadStyle} and ss ${submitStyle}`)
 	    return m("div", {class:"flex flex-column items-center"},[
 		m("h1", "Enter URL to download"),
-	    
+
+		// url text input
 		m("input", {type:"text",
 			    style:"width: 800px",
+			    class:"mb3",
 			    value: url,
 			    autofocus: "autofocus",
 			    onchange: function(e) {
@@ -46,7 +123,8 @@ function downloadComponent() {
 				url = e.currentTarget.value
 			    }
 			   }),
-		m("div", {class:"flex"}, [
+		// radio buttons for format 
+		m("div", {class:"flex mb3"}, [
 		    m("input", {type: "radio",
 				name:"format",
 				value:"mp3",
@@ -70,21 +148,29 @@ function downloadComponent() {
 		    m("div", {class:"mr2"}, "mp4")
 		    
 		]),
+		//submit button
 		m("button", {style: submitStyle,
 			     onclick: function(e) {
-				 e.redraw = false
-				 console.log(this)
+				 //e.redraw = false
 				 callipc(url, format)
+				 url = ""
 			 }
 			     
 			    }, "Submit"),
-		m("div", {class:"loader", style:loadStyle})
+		// loader div
+		m("div", {class:"loader", style: loadStyle})
 	    ]
 		    )
 	    
 	}
     }
 }
+
     
     
-m.mount(document.body, downloadComponent())
+//m.mount(document.body, downloadComponent())
+m.route(document.body, "/download", {
+    "/download": downloadComponent(),
+    "/bulkEdit": bulkEditComponent(),
+    "/singleEdit": singleEditComponent()
+})
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 const electron = require("electron")
-const url = require("url")
-const path = require("path")
 const {app, BrowserWindow, Menu, ipcMain, shell, dialog} = electron;
 const ytdl = require("ytdl-core")
 const fs = require("fs")
 const ytlist = require("youtube-playlist")
 const sanitize = require("sanitize-filename")
+const ffm = require("ffmetadata")
+const ffmpeg = require("fluent-ffmpeg")
 
 var activeStreams = 0
+var activeFiles;
 
 let mainWindow;
 let p = `file://${__dirname}/splash_page.html`
@@ -22,24 +23,40 @@ app.on("ready", function() {
 })
 
 
+function handleError(err) {
+    mainWindow.webContents.send("error", true)
+    dialog.showErrorBox("Oops", err)
+}
+
+
+//#######################################################
+
 function single(url, format, dir) {
     var options = format == "mp3" ? {filter: "audioonly"} : {}
     
     ytdl.getBasicInfo(url, (err, info) => {
 	var title = info.player_response.videoDetails.title
-	//console.log(title)
 	var path = dir + "/" + sanitize(title) + "." + format
 	
 	// to keep track of when finished
 	activeStreams ++
-	
+
+	// ytdl gets a webm file. Same even with audio only (it just strips the video)
 	var reader = ytdl(url, options)
 	var writer = fs.createWriteStream(path)
-	reader.pipe(writer)
+
+	// ffmpeg formats and then pipes out
+	ffmpeg(reader).format(format).pipe(writer)
+	
 	writer.on("finish", () => {
-	    console.log(activeStreams)
+	    // add file to list for editing
+	    activeFiles.push(path)
+	    // decrement open counter
 	    activeStreams --
-	    if (activeStreams == 0) { mainWindow.webContents.send("loading-event", false) }
+	    if (activeStreams == 0) {
+		mainWindow.webContents.send("loading-event", false)
+	    }
+	    
 	})
 
     })
@@ -48,6 +65,7 @@ function single(url, format, dir) {
 
 function playlist(url, format, dir) {
     ytlist(url, "url").then(res => {
+	console.log(`whole playlist = ${res.data.playlist}`)
 	res.data.playlist.forEach(function(videourl) {
 	    console.log(videourl)
 	    single(videourl, format, dir)
@@ -56,8 +74,15 @@ function playlist(url, format, dir) {
 }
 
 function download(url, format, dir) {
-    
+    activeFiles = new Array()
     ytdl.getBasicInfo(url, (err, info) => {
+	console.log(typeof err)
+	if (err != null && !String(err).includes("playlist")) {
+	    console.log(`info = ${info}`)
+	    console.log(`err ${err}`)
+	    handleError(err)
+	    return
+	}
 	if (info === undefined) {
 	    playlist(url, format, dir)
 	}
@@ -77,6 +102,33 @@ function selectDownloadLocation(callback) {
     })
 }
 
+function selectFiles(callback) {
+    const options = {
+	properties: ["openFile", "multiSelections"]
+    }
+    dialog.showOpenDialog(null, options, (filepaths) => {
+	callback(filepaths)
+    })
+}
+
+//####################################################
+
+function addMetaData(file, artist, album, photo) {
+    ffm.read(file, function(err, data) {
+	console.log(`err: ${err} \n data:${JSON.stringify(data)}`)
+	data.artist = artist
+	data.album = album
+	
+	ffm.write(file, data, function(err) {
+	    console.log(`err = ${err} on file=${file}`)
+	})
+    })
+}
+
+
+
+    
+
 //####################################################
 
 
@@ -88,12 +140,23 @@ ipcMain.on("downloadRequest", function(event, args) {
     })
 })
 
+ipcMain.on("bulk-edit", function(event, args) {
+    console.log(`bulk edit event recieve. art:${args.artist} alb:${args.album}, photo:${args.photo}`)
+    activeFiles.forEach( (file) => addMetaData(file, args.artist, args.album, args.photo))
+})
+
 
 //####################################################
 
 const menu = [
     {
 	label: "Open",
+	click: function() {
+	    selectFiles((files) => {
+		activeFiles = files
+		mainWindow.webContents.send("openBulk")
+	    })
+	},
     },
     {
 	label: "About",
