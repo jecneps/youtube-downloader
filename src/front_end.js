2c9a1bbfb13cd3959f8b73bd7566827c28ff138f
jecneps <jejcneps@gmail.com>
2019-06-26 17:16:39 -0400

    YEEEET!

--- src/front_end.js
+++ src/front_end.js
@@ -1,5 +1,5 @@
 const m = require("mithril")
-const ipc = require("electron").ipcRenderer
+
 
 const LoadEnum = {
     start: 0,
@@ -7,42 +7,126 @@ const LoadEnum = {
 }
 
 var loadState = LoadEnum.start
+let ipc;
+var activeFiles = new Array()
 
-ipc.on("loading-event", function(event, args) {
+//#############################################
+class File {
+    constructor(dir, name, meta) {
+	this.dir = dir
+	this.name = name
+	this.meta = meta
+	this.isBulk = dir == null && name == null
+    }
+}
+
+//#################################################
+// IPC
+//################################################
+
+class IPC {
+    constructor() {
+	this.ipc = require("electron").ipcRenderer
+    }
+
+    on(channel, fun) {
+	this.ipc.on(channel, fun)
+    }
+
+    downloadRequest(url, format) {
+	this.ipc.send("downloadRequest", {url: url,
+					  format: format})
+    }
+
+    editMetaRequest(file) {
+	this.ipc.send("editMetaRequest", {file: file})
+    }
+
+    updateFilesRequest() {
+	this.ipc.send("updateFilesRequest", {})
+    }
+
+    deleteRequest(path) {
+	this.ipc.send("deleteRequest", {path: path})
+    }
+
+    clearState() {
+	this.ipc.send("clearState", {})
+    }
+
+}
+
+function loadingEvent(event, args) {
     console.log(event)
     console.log(args)
-    if (args) { loadState = LoadEnum.loading }
+    console.log(args.isLoading)
+    if (args.isLoading) {
+	console.log("isloading")
+	loadState = LoadEnum.loading
+    }
     else {
+	console.log("here")
 	loadState = LoadEnum.start
 	m.route.set("/bulkEdit")
     }
     m.redraw()
-})
+}
 
-ipc.on("error", function(event, args) {
+function error(event, args) {
     console.log("EEERRROR")
+    console.log(args.error)
     loadState = LoadEnum.start
     m.redraw()
-})
+}
 
-ipc.on("openBulk", function(event, args) {
-    m.route.set("/bulkEdit")
-})
+function setRoute(event, args) {
+    m.route.set(args.route)
+}
 
-function callipc(url, format) {
-    ipc.send("downloadRequest", {
-	url: url,
-	format: format,
-    })
-    console.log(`sent ${url} and ${format}`)
+function updateActiveFiles(event, args) {
+    console.log(`new files from main:${JSON.stringify(args.files)}`)
+    activeFiles = args.files
+    m.redraw()
+}
+//###################################################
+//###################################################
+
+var curData = {
+    show: false,
+    artist: "",
+    album: "",
+    title: "",
+    curFile: null
+}
+
+function curMeta() {
+    return {
+	artist: curData.artist,
+	album: curData.album,
+	title: curData.title
+    }
 }
 
 function makeCards() {
     var cards = new Array()
     activeFiles.forEach(function(file) {
-	var comp = m("div", {class:""}, [
-	    m("span", file.replace(".mp3", "")),
-	    m("span", "XX")
+	console.log(`file in cards:${JSON.stringify(file)}`)
+	var comp = m("div", {class:"bg-washed-red mt2 mb2"}, [
+	    m("span", {class: "f5",
+		       onclick: function() {
+			   curData.title = file.meta.title
+			   curData.artist = file.meta.artist
+			   curData.album = file.meta.album
+			   curData.curFile = file
+			   curData.show = true
+		       }
+		      }, file.name.replace(".mp3", "")),
+	    m("button", {class:"ml3 fr",
+			 onclick: function(){
+			     ipc.deleteRequest(file)
+			     ipc.updateFilesRequest()
+			 }
+			}, "X")
 	])
 	cards.push(comp)
     })
@@ -54,61 +138,101 @@ function makeCards() {
 function singleEditComponent() {
     return {
 	view: function() {
-	    return m("div", {class: "flex flex-column items-center",
+	    var main = m("div", {class: "bg-near-white flex flex-column items-center",
 			     style: "margin: auto"
 			    }, [
 				m("div", {class: "flex flex-column"}, makeCards()),
-				m("button", {class: ""}, "Finish")
+				m("button", {class: "",
+					     onclick: function() {
+						 ipc.clearState()
+						 m.route.set("/download")
+					     }
+					    }, "Finish")
 			    ])
+	    return curData.show ? [main, singleEditPopup()] : main
 	}
     }
 }
 
+function editField(title, dict, key, prefill) {
+    var text = prefill == undefined ? "" : prefill
+    return [m("p", title),
+	    m("input",{type: "text",
+		       class: "mb4",
+		       onchange: function(e) {
+			   e.redraw = false
+			   dict[key] = e.currentTarget.value
+		       },
+		       value: text
+		      })
+	    ]
+}
+
+function singleEditPopup() {
+    return m("div", {class: "flex flex-column bg-orange w-50", style: "margin: auto"}, [
+	m("h1", "Single Edit") ,
+	m("div", {class: "flex"}, [
+	    m("div", {class: "flex flex-column"}, [
+		    ...editField("Title", curData, "title", curData.title),
+		    ...editField("Artist", curData, "artist", curData.artist),
+		    ...editField("Album", curData, "album", curData.album)
+	    ])
+	    // TODO: image thing
+	]),
+	m("div", {class: "flex"}, [
+	    m("button", {class: "",
+			 onclick: function() {
+			     curData.show = false
+			 }
+			},
+	      "Cancel"),
+	    m("button", {class: "",
+			 onclick: function() {
+			     curData.show = false
+			     curData.curFile.meta = curMeta()
+			     ipc.editMetaRequest(curData.curFile)
+			     // TODO have this effect file name and reshape
+			     
+			     console.log("clicked save")
+			 }
+			},
+	      "Save")
+	])
+    ])   
+}
 
 function bulkEditComponent() {
-    let artist = "";
-    let album = "";
-    let photo = null;
+    var data = {
+	artist: "",
+	album: "",
+    }
     return {
 	view: function() {
 	    return m("div", {class: "flex flex-column bg-orange w-50", style: "margin: auto"}, [
 		m("h1", "Bulk Edit") ,
 		m("div", {class: "flex"}, [
 		    m("div", {class: "flex flex-column"}, [
-			m("p", "Artist"),
-			m("input", {type: "text",
-				    class: "mb4",
-				    onchange: function(e) {
-					e.redraw = false
-					artist = e.currentTarget.value
-				    }
-				   }),
-			m("p", "Album"),
-			m("input", {type: "text",
-				    class: "",
-				    onchange: function(e) {
-					e,redraw = false
-					album = e.currentTarget.value
-				    }
-				   })
-			
+			    ...editField("Artist", data, "artist"),
+			    ...editField("Album", data, "album")			
 		    ]),
-		    m("div", "Image thing")
+		    //TODO: IMAGE
 		]),
 		m("div", {class: "flex"}, [
 		    m("button", {class: "",
 				 onclick: function() {
+				     ipc.updateFilesRequest()
 				     m.route.set("/singleEdit")
 				 }
 				},
 				 "Skip"),
 		      m("button", {class: "",
 				   onclick: function() {
-				       ipc.send("bulk-edit", {
-					   artist: artist,
-					   album: album,
-					   photo: photo
-				       })
+				       var file = new File(null,
+								    null,
+								    {artist: data.artist,
+								     album: data.album})
+				       console.log(`bulk edit, file=${JSON.stringify(file)}`)
+				       ipc.editMetaRequest(file)
 				       m.route.set("/singleEdit")
 				   }
 				  },
@@ -126,10 +250,10 @@ function downloadComponent() {
     let format = "mp3"
     return  {
 	view: function() {
-	    console.log(`load state ${loadState}`)
+	   // console.log(`load state ${loadState}`)
 	    var loadStyle = (loadState == LoadEnum.loading) ? "display:block" : "display:none"
 	    var submitStyle = (loadState == LoadEnum.loading) ? "display:none":"display:block"
-	    console.log(`ls ${loadStyle} and ss ${submitStyle}`)
+	   // console.log(`ls ${loadStyle} and ss ${submitStyle}`)
 	    return m("div", {class:"flex flex-column items-center"},[
 		m("h1", "Enter URL to download"),
 
@@ -173,7 +297,7 @@ function downloadComponent() {
 		m("button", {style: submitStyle,
 			     onclick: function(e) {
 				 //e.redraw = false
-				 callipc(url, format)
+				 ipc.downloadRequest(url, format)
 				 url = ""
 			 }
 			     
@@ -187,11 +311,23 @@ function downloadComponent() {
     }
 }
 
+
+function setup() {
+    //m.mount(document.body, downloadComponent())
+    m.route(document.body, "/download", {
+	"/download": downloadComponent(),
+	"/bulkEdit": bulkEditComponent(),
+	"/singleEdit": singleEditComponent()
+    })
+
+    ipc = new IPC()
+    ipc.on("loading-event", loadingEvent)
+    ipc.on("error", error)
+    ipc.on("update-files", updateActiveFiles)
+    ipc.on("set-route", setRoute)
+
+}
+
+setup()
     
-    
-//m.mount(document.body, downloadComponent())
-m.route(document.body, "/download", {
-    "/download": downloadComponent(),
-    "/bulkEdit": bulkEditComponent(),
-    "/singleEdit": singleEditComponent()
-})
+
--- src/main.js
+++ src/main.js
@@ -1,30 +1,80 @@
 const electron = require("electron")
-const {app, BrowserWindow, Menu, ipcMain, shell, dialog} = electron;
+const {app, BrowserWindow, Menu, shell, dialog} = electron;
 const ytdl = require("ytdl-core")
 const fs = require("fs")
 const ytlist = require("youtube-playlist")
 const sanitize = require("sanitize-filename")
 const ffm = require("ffmetadata")
 const ffmpeg = require("fluent-ffmpeg")
+const Path = require("path")
+
+const SPLASH_PATH = `file://${__dirname}/splash_page.html`
+
+
+
 
 var activeStreams = 0
-var activeFiles;
+let activeFiles;
+let ipc;
+var mainWindow;
 
-let mainWindow;
-let p = `file://${__dirname}/splash_page.html`
-app.on("ready", function() {
-    mainWindow = new BrowserWindow({
-	webPreferences: {
-	    nodeIntegration: true
-	}
+
+const menu = [
+    {
+	label: "Open",
+	click: function() {
+	    selectFiles((files) => {
+		console.log(files)
+		activeFiles = new Array()
+		files.forEach(function(file) {
+		    pathToFileObj(file, activeFiles)
+		})
+		ipc.setRoute("/bulkEdit")
+	    })
+	},
+    },
+    {
+	label: "About",
+    },
+    {
+	label: "debug",
+	click: function() {
+	    mainWindow.webContents.openDevTools()
+	},
+    }
+]
+
+//######################################################
+//######################################################
+function bug(s) {
+    console.log("\n" + s + "\n")
+}
+
+// asyncronously turn into File obj and put in array
+function pathToFileObj(path, trgArray) {
+    var meta = ffm.read(path, function(err, data) {
+	trgArray.push(new File(Path.dirname(path) + "/",
+			       Path.basename(path),
+			       data))
     })
-    mainWindow.loadURL(p)
-    mainWindow.maximize()
-})
+}
+
+class File {
+    constructor(dir, name, meta) {
+	this.dir = dir
+	this.name = name
+	this.meta = meta
+	this.isBulk = dir == null && name == null
+    }
+}
+
+
+//#######################################################
+//#######################################################
 
 
 function handleError(err) {
-    mainWindow.webContents.send("error", true)
+    ipc.error("error", err)
     dialog.showErrorBox("Oops", err)
 }
 
@@ -35,8 +85,9 @@ function single(url, format, dir) {
     var options = format == "mp3" ? {filter: "audioonly"} : {}
     
     ytdl.getBasicInfo(url, (err, info) => {
-	var title = info.player_response.videoDetails.title
-	var path = dir + "/" + sanitize(title) + "." + format
+	var title = sanitize(info.player_response.videoDetails.title)
+	var name = title + "." + format
+	var path = dir + "/" + name
 	
 	// to keep track of when finished
 	activeStreams ++
@@ -50,11 +101,11 @@ function single(url, format, dir) {
 	
 	writer.on("finish", () => {
 	    // add file to list for editing
-	    activeFiles.push(path)
+	    activeFiles.push(new File(dir + "/", name, {}))
 	    // decrement open counter
 	    activeStreams --
 	    if (activeStreams == 0) {
-		mainWindow.webContents.send("loading-event", false)
+		ipc.loadState(false)	
 	    }
 	    
 	})
@@ -113,14 +164,45 @@ function selectFiles(callback) {
 
 //####################################################
 
-function addMetaData(file, artist, album, photo) {
-    ffm.read(file, function(err, data) {
-	console.log(`err: ${err} \n data:${JSON.stringify(data)}`)
-	data.artist = artist
-	data.album = album
+function isValidMeta(data) {
+    return data != undefined && data != ""
+}
+
+function updateMeta(oldMeta, newMeta) {
+    if (isValidMeta(newMeta.artist)) { oldMeta.artist = newMeta.artist }
+    if (isValidMeta(newMeta.album)) { oldMeta.album = newMeta.album }
+    if (isValidMeta(newMeta.title)) { oldMeta.title = newMeta.title }
+}
+
+
+// File * meta obj -> ()
+// treats file as same one in active files
+function addMetaData(file, meta) {
+    bug(`file=${JSON.stringify(file)}, meta:${JSON.stringify(meta)}`)
+    ffm.read(file.dir + file.name, function(err, data) {
+	bug(`err: ${err}`)
+	bug(`data before:${JSON.stringify(data)}`)
+	updateMeta(data, meta)
+	bug(`data after switch:${JSON.stringify(data)}`)
 	
-	ffm.write(file, data, function(err) {
-	    console.log(`err = ${err} on file=${file}`)
+	ffm.write(file.dir + file.name, data, function(err) {
+	    file.meta = data
+	    if (isValidMeta(data.title)) {
+		bug("in write, data.title was valid")
+		// REWRITE TITLE IF IT HAS
+		
+		fs.renameSync(file.dir + file.name, file.dir + data.title + ".mp3")
+		// edit file in local storage
+		file.name = data.title + ".mp3"
+		bug(`file after rewriting name:${JSON.stringify(file)}`)
+		// this only happens on single edit, so updte immediately
+		
+	    }
+	    bug("update comparios")
+	    bug(`file that might have changed:${JSON.stringify(file)}`)
+	    bug(`current activeFiles:${JSON.stringify(activeFiles)}`)
+	    ipc.updateActiveFiles(activeFiles)
+	    bug(`err = ${err} on file=${JSON.stringify(file)}`)
 	})
     })
 }
@@ -130,44 +212,107 @@ function addMetaData(file, artist, album, photo) {
     
 
 //####################################################
+// IPC
+//####################################################
 
+class IPC {
+    constructor(window) {
+	this.ipc = require("electron").ipcMain
+	console.log(`constructor window=${window}`)
+	this.window = window
+    }
 
-ipcMain.on("downloadRequest", function(event, args) {   
+    on(channel, fun) {
+	this.ipc.on(channel, fun)
+    }
+
+    setRoute(route) {
+	this.window.webContents.send("set-route", {route: route})
+    }
+
+    updateActiveFiles(files) {
+	this.window.webContents.send("update-files", {files: files})
+    }
+
+    loadState(isLoading) {
+	console.log(`loadState this=${this}`)
+	this.window.webContents.send("loading-event", {isLoading: isLoading})
+    }
+
+    error(err) {
+	this.window.webContents.send("error", {error: err})
+    }
+}
+
+function downloadRequest(event, args) {
     selectDownloadLocation((dir) => {
 	// Tell Ui to load
-	mainWindow.webContents.send("loading-event", true)
+	ipc.loadState(true)
 	download(args.url, args.format, dir)
     })
-})
+}
 
-ipcMain.on("bulk-edit", function(event, args) {
-    console.log(`bulk edit event recieve. art:${args.artist} alb:${args.album}, photo:${args.photo}`)
-    activeFiles.forEach( (file) => addMetaData(file, args.artist, args.album, args.photo))
-})
+function editMetaRequest(event, args) {
+    bug(`activeFiles:${JSON.stringify(activeFiles)}`)
+    bug(`args: ${JSON.stringify(args)}`)
+    bug(`recieved meta edit req, file:${JSON.stringify(args.file)}`)
+    if (args.file.isBulk) {
+	console.log("is not")
+	activeFiles.forEach((file) => {
+	    console.log(`From active files: ${JSON.stringify(file)}`)
+	    addMetaData(file, args.file.meta)
+	})
+    }
+    else {
+	console.log("is single")
+	var file = activeFiles.find((f) => f.dir == args.file.dir && f.name == args.file.name)
+	addMetaData(file, args.file.meta)
+    }
+}
+
+function deleteFileRequest(event, args) {
+    fs.unlink(args.path, (err) => {
+	console.log(err)
+    })
+    activeFiles = activeFiles.filter((file) => filename != args.path)
+}
+
+function clearState(event, args) {
+    activeFiles = null
+    activeStreams = 0
+}
+
 
 
 //####################################################
 
-const menu = [
-    {
-	label: "Open",
-	click: function() {
-	    selectFiles((files) => {
-		activeFiles = files
-		mainWindow.webContents.send("openBulk")
-	    })
-	},
-    },
-    {
-	label: "About",
-    },
-    {
-	label: "debug",
-	click: function() {
-	    mainWindow.webContents.openDevTools()
-	},
-    }
-]
 
-Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
 
+
+
+function setup() {
+    // create main window
+    app.on("ready", function() {
+	
+	mainWindow = new BrowserWindow({
+	    webPreferences: {
+		// allows us to use webpack and requires on the render side
+		nodeIntegration: true
+	    }
+	})
+	mainWindow.loadURL(SPLASH_PATH)
+	mainWindow.maximize()
+
+	ipc = new IPC(mainWindow)
+	ipc.on("downloadRequest", downloadRequest)
+	ipc.on("editMetaRequest", editMetaRequest)
+	ipc.on("deleteRequest", deleteFileRequest)
+	ipc.on("clearState", clearState)
+	ipc.on("updateFilesRequest", (event, args) => ipc.updateActiveFiles(activeFiles))
+    })
+
+    // set menu stuff
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menu))  
+}
+
+setup()
